d8903e97de7cd49188eba53006a84ac09fd5cba4
nissaadila <nissaadila15@gmail.com>
2022-09-04 21:27:20 +0700

    Splash Screen

---
+++ app/src/main/java/com/example/tourin/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.tourin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+    private  static  int SPLASH_SCREEN =  5000;
+
+    Animation topAnim , bottomAnim;
+    ImageView imageLogo, textLogo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        imageLogo = findViewById(R.id.logoTourIn);
+        textLogo = findViewById(R.id.textSplash);
+
+        imageLogo.setAnimation(topAnim);
+        textLogo.setAnimation(bottomAnim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent move = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(move);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+}
