afa04c5657eacd35372613d7154e0cbbd0f027da
nissaadila <nissaadila15@gmail.com>
2022-09-05 18:05:26 +0700

    Navbar & UI profile fragment

--- app/src/main/java/com/example/tourin/MainActivity.java
+++ app/src/main/java/com/example/tourin/MainActivity.java
@@ -1,25 +1,69 @@
 package com.example.tourin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnLogout;
+
+    MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogout = findViewById(R.id.btnLogoutMain);
-        btnLogout.setOnClickListener(v -> {
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(this, LoginActivity.class));
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_save));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.ic_search));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.ic_profile));
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment fragment;
+
+                if(item.getId()==4){
+                    fragment = new ProfileFragment();
+                }else if(item.getId()==3){
+                    fragment = new SearchFragment();
+                }else if(item.getId()==2){
+                    fragment = new SavedFragment();
+                }else{
+                    fragment = new HomeFragment();
+                }
+                loadFragment(fragment);
+            }
         });
+
+        bottomNavigation.show(1,true);
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+
+            }
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+
+            }
+        });
+
+
+    }
+    private void loadFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,fragment,null).commit();
     }
 }
