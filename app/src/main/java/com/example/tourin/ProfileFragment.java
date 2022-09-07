1c4daa60d4baff906d9beb569405369bd55a0164
nissaadila <nissaadila15@gmail.com>
2022-09-07 23:30:41 +0700

    current user profile belom jalan

--- app/src/main/java/com/example/tourin/ProfileFragment.java
+++ app/src/main/java/com/example/tourin/ProfileFragment.java
@@ -3,20 +3,45 @@ package com.example.tourin;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
     private Button btnLogoutMain;
+    TextView tvUsername,tvEmail;
 
+    final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tourin-839e2-default-rtdb.firebaseio.com/");
     public ProfileFragment() {
         // Required empty public constructor
+
+    }
+
+    public static ProfileFragment newInstance() {
+
+        Bundle args = new Bundle();
+        ProfileFragment fragment = new ProfileFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -24,13 +49,42 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_profile, container, false);
 
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+
+        tvUsername = view.findViewById(R.id.textUserName);
+        tvEmail = view.findViewById(R.id.textUserEmail);
         btnLogoutMain = view.findViewById(R.id.btnLogoutMain);
+
+//        Query query = databaseReference.orderByChild("email").equalTo(firebaseUser.getEmail());
+
+        databaseReference.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    String username = dataSnapshot.child("name").getValue().toString();
+                    String email = dataSnapshot.child("email").getValue().toString();
+
+                    tvUsername.setText(username);
+                    tvEmail.setText(email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
         btnLogoutMain.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
             startActivity(new Intent(getContext(), LoginActivity.class));
         });
 
+
         return view;
     }
+
 }
